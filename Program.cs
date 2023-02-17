ba561c8c8811a5e27bc8bc405b4f4f85f25313c9
Ivan Pivnev <pivnev-1998@mail.ru>
2023-02-17 20:32:19 +0300

    Пример 1 написали

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// Напишите программу, которая принимает на вход координаты точки 
+// (X и Y), причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, 
+// в которой находится эта точка.
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message}> ");
+    int value;
+    if (int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
+
+bool ValidateCoords(int x, int y)
+{
+    if (x == 0 || y == 0)
+    {
+        System.Console.WriteLine("Одна из координат находится на оси");
+        return false;
+    }
+
+    return true;
+}
+
+int GetQuorter(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+    return 4;
+}
+
+int x = InputInt("Введите X");
+int y = InputInt("Введите Y");
+if (ValidateCoords(x, y))
+{
+    System.Console.WriteLine($"Координаты {x}, {y} находятся в четверти {GetQuorter(x, y)}");
+}
